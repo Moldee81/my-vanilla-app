3218ed95cffb53f7747e4cb7f33769fe76771157
Moldee81 <a.penatomczak@gmail.com>
2025-01-24 13:17:48 +0000

    added API key

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+function searchCity(city) {
+  let apiKey = "6dod2fbfa8c43fe552ftae49bc36d90b";
+  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
